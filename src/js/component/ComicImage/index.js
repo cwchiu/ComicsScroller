e787e5b633dfc2882a830e79d1f8163b399ae05a
arick <arick@grr.la>
2020-09-05 01:57:20 +0800

    refactor: React Hook 重構

---
+++ src/js/component/ComicImage/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import cn from './ComicImage.css';
+import { updateImgType } from '../../reducers/comics';
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function getImgClass(type) {
+  switch (type) {
+    case 'normal':
+      return cn.ComicImage;
+    case 'wide':
+      return cn.ComicImageWide;
+    case 'natural':
+      return cn.ComicImageNatural;
+    case 'end':
+      return cn.ComicImageEnd;
+    default:
+      return cn.ComicImageInit;
+  }
+}
+const ComicImage = ({ index }) => {
+  const [showImage, setShowImage] = useState(false);
+  const dispatch = useDispatch();
+  const { src, loading, type, height, innerHeight } = useSelector(state => {
+    const { comics } = state;
+    const { src, loading, type, height } = comics.imageList.entity[index];
+    return { src, loading, type, height, innerHeight: comics.innerHeight };
+  });
+  const imgLoadHandler = (e) => {
+    if (type === 'image' && e.target instanceof HTMLImageElement) {
+      const w = e.target.naturalWidth;
+      const h = e.target.naturalHeight;
+      if (h > innerHeight - 48) {
+        if (w > h) {
+          dispatch(updateImgType(innerHeight - 68, index, 'wide'));
+        } else {
+          dispatch(updateImgType(h + 4, index, 'natural'));
+        }
+      } else {
+        dispatch(updateImgType(h + 4, index, 'natural'));
+      }
+    }
+    setShowImage(true);
+  };
+
+  return (
+    <div
+      className={getImgClass(type)}
+      style={{
+        height:
+          type === 'wide'
+            ? innerHeight - 68
+            : height,
+      }}
+    >
+      {!showImage && type !== 'end' ? (
+        <div>Loading...</div>
+      ) : (
+          undefined
+        )}
+      {!loading && type !== 'end' ? (
+        <img
+          style={showImage ? undefined : { display: 'none' }}
+          src={src}
+          onLoad={imgLoadHandler}
+          alt={'comicImage'}
+        />
+      ) : (
+          undefined
+        )}
+      {type === 'end' ? '本 章 結 束' : undefined}
+    </div>
+  );
+};
+
+ComicImage.propTypes = {
+  index: PropTypes.number.isRequired,
+};
+
+export default ComicImage;
