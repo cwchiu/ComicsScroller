57960e58d05a173dc2f30f611dd0eee2965a24ed
arick <arick@grr.la>
2020-09-11 00:11:27 +0800

    refactot: 移除非必要程式

--- src/js/component/ComicImage/index.js
+++ src/js/component/ComicImage/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import cn from './ComicImage.css';
 import { updateImgType } from '../../reducers/comics';
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 import PropTypes from 'prop-types';
 
 function getImgClass(type) {
@@ -22,8 +21,7 @@ function getImgClass(type) {
 const ComicImage = ({ index }) => {
   const [showImage, setShowImage] = useState(false);
   const dispatch = useDispatch();
-  const { src, loading, type, height, innerHeight } = useSelector(state => {
-    const { comics } = state;
+  const { src, loading, type, height, innerHeight } = useSelector(({ comics }) => {
     const { src, loading, type, height } = comics.imageList.entity[index];
     return { src, loading, type, height, innerHeight: comics.innerHeight };
   });
@@ -31,12 +29,8 @@ const ComicImage = ({ index }) => {
     if (type === 'image' && e.target instanceof HTMLImageElement) {
       const w = e.target.naturalWidth;
       const h = e.target.naturalHeight;
-      if (h > innerHeight - 48) {
-        if (w > h) {
-          dispatch(updateImgType(innerHeight - 68, index, 'wide'));
-        } else {
-          dispatch(updateImgType(h + 4, index, 'natural'));
-        }
+      if (h > innerHeight - 48 && w > h) {
+        dispatch(updateImgType(innerHeight - 68, index, 'wide'));
       } else {
         dispatch(updateImgType(h + 4, index, 'natural'));
       }
@@ -54,22 +48,18 @@ const ComicImage = ({ index }) => {
             : height,
       }}
     >
-      {!showImage && type !== 'end' ? (
+      {(!showImage && type !== 'end') && (
         <div>Loading...</div>
-      ) : (
-          undefined
-        )}
-      {!loading && type !== 'end' ? (
+      )}
+      {(!loading && type !== 'end') && (
         <img
           style={showImage ? undefined : { display: 'none' }}
           src={src}
           onLoad={imgLoadHandler}
-          alt={'comicImage'}
+          alt='comicImage'
         />
-      ) : (
-          undefined
-        )}
-      {type === 'end' ? '本 章 結 束' : undefined}
+      )}
+      {(type === 'end') && '本 章 結 束'}
     </div>
   );
 };
